fc190158eabe30694d68e647ef2d0b5fad5021f5
Adarsh Gopalakrishnan <agkrishnan2003@gmail.com>
2022-07-15 13:42:15 +0530

    Nav for desktop done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
+  purge: ['./src/**/*.js', './public/index.html'],
   content: ["./index.html"],
   theme: {
     screens: {
-      'mobile': {'max': '448px'}
+      'mobile': {'max': '448px'},
+      'desktop': {'min': '448px'}
     },
     colors: {
       'lightest': '#FFF2F2',
